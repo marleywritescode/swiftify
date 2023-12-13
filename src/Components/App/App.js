0589def261d4c3972d37276961e0b967b2646d1c
Marley Morrell <Marley.Morrell@silabs.com>
2023-12-13 10:46:01 -0500

    convert app to functional

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import "./App.css";
 
@@ -9,122 +9,172 @@ import PlayList from "../Playlist/Playlist";
 import Spotify from "../../util/Spotify";
 import Footer from "../Footer/Footer";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+export default () => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [playlistTracks, setPlaylistTracks] = useState([]);
+    const [playlistName, setPlaylistName] = useState('');
+    const [showLogin, setShowLogin] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    this.state = {
-      searchResults: [],
-      playlistName: "",
-      playlistTracks: [],
-      showLogin: true,
-      searchTerm: "",
-    };
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-    this.updatePlaylistName = this.updatePlaylistName.bind(this);
-    this.savePlaylist = this.savePlaylist.bind(this);
-    this.search = this.search.bind(this);
-  }
+//   constructor(props) {
+//     super(props);
 
-  checkLogin() {
-    let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    if (accessTokenMatch) {
-      this.setState({ showLogin: false });
-      Spotify.getAccessToken();
-    }
-  }
+//     this.state = {
+//       searchResults: [],
+//       playlistName: "",
+//       playlistTracks: [],
+//       showLogin: true,
+//       searchTerm: "",
+//     };
+//     this.addTrack = this.addTrack.bind(this);
+//     this.removeTrack = this.removeTrack.bind(this);
+//     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+//     this.savePlaylist = this.savePlaylist.bind(this);
+//     this.search = this.search.bind(this);
+//   }
 
-  login() {
-    Spotify.login();
-  }
-
-  addTrack(track) {
-    let tracks = this.state.playlistTracks;
-    let searchResults = this.state.searchResults;
-    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
-      return;
-    } else {
-      tracks.push(track);
-      this.setState({ playlistTracks: tracks });
-      for (let i = searchResults.length - 1; i >= 0; i--) {
-        if (searchResults[i] === track) {
-          searchResults.splice(i, 1);
+//   const checkLogin = useCallback(
+//     () => {
+//         let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+//         if (accessTokenMatch) {
+//         // this.setState({ showLogin: false });
+//             setShowLogin(false);
+//             Spotify.getAccessToken();
+//         }
+//     },
+//     [],
+//   );
+useEffect(
+    () => {
+        let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        if (accessTokenMatch) {
+        // this.setState({ showLogin: false });
+            setShowLogin(false);
+            Spotify.getAccessToken();
         }
-      }
+    },
+    [],
+  );
 
-      this.setState({ searchResults: searchResults });
+  const login = () => {
+    Spotify.login();
+  };
+
+  const addTrack = useCallback(
+    track => {
+        let tracks = playlistTracks;
+        let searchresults = searchResults;
+        if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+            return;
+        } else {
+            tracks.push(track);
+            // this.setState({ playlistTracks: tracks });
+            setPlaylistTracks(tracks);
+            for (let i = searchResults.length - 1; i >= 0; i--) {
+                if (searchResults[i] === track) {
+                searchResults.splice(i, 1);
+                }
+            }
+
+            // this.setState({ searchResults: searchResults });
+            setSearchResults(searchresults);
+        }
+    },
+    [playlistTracks, searchResults],
+  );
+
+  const removeTrack = useCallback(
+    track => {
+        let tracks = playlistTracks;
+        tracks.splice(track, 1);
+        // this.setState({ playlistTracks: tracks });
+        setPlaylistTracks(tracks);
+    },
+    [playlistTracks],
+  );
+
+  const updatePlaylistName = playlistname => setPlaylistName(playlistname);
+
+  const savePlaylist = useCallback(
+    () => {
+    const trackURIs = playlistTracks.map((track) => track.uri);
+        Spotify.savePlaylist(
+            playlistName,
+            trackURIs,
+            ).then(
+                () => {
+                    // this.setState({
+                    //     playlistName: "New Playlist",
+                    //     playlistTracks: [],
+                    // });
+                    setPlaylistName('New Playlist');
+                    setPlaylistTracks([]);
+                },
+            );
+    },
+    [playlistTracks],
+  );
+
+  const search = useCallback(
+    term => {
+        Spotify.search(term).then(
+            (searchresults) => {
+            setSearchResults(serachresults);
+            // this.setState({ searchResults: searchResults });
+            },
+        );
+        // this.setState({ searchTerm: term });
+        setSearchTerm(term);
+    },
+    [],
+  );
+
+//   componentDidMount() {
+//     this.checkLogin();
+//   }
+
+return <div className="body">
+    {showLogin
+        ? <>
+            <div className="titleheader">
+                <h1>
+                Swiftify
+                <p>Reclaim your spotify playlists</p>
+                </h1>
+            </div>
+            <div className="App">
+                {showLogin && <Login onClick={login} />}
+            </div>
+        </>
+        : <>
+            <div className="titleheader">
+                <h1>
+                Swiftify
+                <p>Reclaim your spotify playlists</p>
+                </h1>
+            </div>
+            <div className="App">
+                {showLogin && <Login onClick={login} />}
+                {!showLogin && <>
+                    <SearchBar onSearch={search} />
+                    <div className="App-playlist">
+                    <SearchResults
+                        searchResults={searchResults}
+                        onAdd={addTrack}
+                    />
+
+                    <PlayList
+                        playlistName={playlistName}
+                        playlistTracks={playlistTracks}
+                        onRemove={removeTrack}
+                        onNameChange={updatePlaylistName}
+                        onSave={savePlaylist}
+                    />
+                    </div>
+                </>}
+            </div>
+        </>
     }
-  }
-
-  removeTrack(track) {
-    let tracks = this.state.playlistTracks;
-    tracks.splice(track, 1);
-    this.setState({ playlistTracks: tracks });
-  }
-
-  updatePlaylistName(name) {
-    this.setState({ playlistName: name });
-  }
-
-  savePlaylist() {
-    const trackURIs = this.state.playlistTracks.map((track) => track.uri);
-    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
-      this.setState({
-        playlistName: "New Playlist",
-        playlistTracks: [],
-      });
-    });
-  }
-
-  search(term) {
-    Spotify.search(term).then((searchResults) => {
-      this.setState({ searchResults: searchResults });
-    });
-    this.setState({ searchTerm: term });
-  }
-
-  componentDidMount() {
-    this.checkLogin();
-  }
-
-  render() {
-    return (
-      <div className="body">
-        <div className="titleheader">
-          <h1>
-            Taylorify
-            <p>Reclaim your spotify playlists</p>
-          </h1>
-        </div>
-        <div className="App">
-          {this.state.showLogin && <Login onClick={this.login} />}
-          {!this.state.showLogin && (
-            <>
-              <SearchBar onSearch={this.search} />
-              <div className="App-playlist">
-                <SearchResults
-                  searchResults={this.state.searchResults}
-                  onAdd={this.addTrack}
-                />
-
-                <PlayList
-                  playlistName={this.state.playlistName}
-                  playlistTracks={this.state.playlistTracks}
-                  onRemove={this.removeTrack}
-                  onNameChange={this.updatePlaylistName}
-                  onSave={this.savePlaylist}
-                />
-              </div>
-            </>
-          )}
-          
-        </div>
-        <Footer />  
-      </div>
-      
-    );
-  }
-}
-
-export default App;
+    <Footer />  
+</div>;
+};
